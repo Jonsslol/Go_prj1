23431956099b21a15c38317872b8f31358d21d0f
João Victor <jvquadros2010@hotmail.com>
2024-03-03 22:22:48 -0300

    feat: create the database

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
@@ -10,7 +12,20 @@ var (
 )
 
 func Inti() error {
+	var err error
+
+	// Initialize SQLite
+	db, err = initializeSQLite()
+
+	if err != nil {
+		return fmt.Errorf("error initializing sqlite %v", err)
+	}
+
 	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
 
 func Getlogger(p string) *Logger {
